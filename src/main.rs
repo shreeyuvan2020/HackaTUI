e9b84610d83ab64904b11f2be7c5d1ad0cbd4803
shreeyuvan2020 <shreeyuvan2020@gmail.com>
2026-06-11 13:55:34 +0530

    add gitignore and other package stuff

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use reqwest::Client;
+use ratatui_plt::{heatmap_widget, prelude::*};
+use ratatui::{Frame, GridData};
+pub fn spans_data(username:&str, start_date, end_date) {
+    let client = reqwest::Client::new();
+    let request = client.get("https://hackatime.hackclub.com/api/v1/users/{username}/heartbeats/spans?start_date=&end_date=&project=&filter_by_project=");
+    let response = request.send().await?;
+}
+fn run(terminal: &mut ratatui::DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(|frame| render(frame))?;
+        if handle_events()? {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    let data = GridData::from_fn((-1.0, 1.0), (-1.0, 1.0), 20, 20, |x, y| x * y);
+    let h = heatmap_widget!(data);
+    frame.render_widget(h, frame.area());
+}
